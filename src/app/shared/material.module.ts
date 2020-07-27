9908f76bdc6bc888bbf3afec63cac309e5c99c55
Maxim <astralfestival@gmail.com>
2020-07-27 23:54:26 +0300

    trying to add sentry

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -2,6 +2,7 @@ import { ObserversModule } from "@angular/cdk/observers";
 import { OverlayModule } from "@angular/cdk/overlay";
 import { PortalModule } from "@angular/cdk/portal";
 import { NgModule } from "@angular/core";
+import { MatBottomSheetModule } from "@angular/material/bottom-sheet";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatRippleModule } from "@angular/material/core";
@@ -20,6 +21,7 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatTooltipModule } from "@angular/material/tooltip";
 
 const matModules = [
+  MatBottomSheetModule,
   MatButtonModule,
   MatCardModule,
   MatDialogModule,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  dsn: process.env.SENTRY_DSN,
 };
